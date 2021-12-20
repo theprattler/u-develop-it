d112e94e308fb09b83a80be89280c122e40c9ecb
The Prattler <jmichaelsmith@juno.com>
2021-12-20 12:00:05 -0500

    connected to mysql

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Shaggy307!',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+})
 
+// Catchall 404 error
 app.use((req, res) => {
   res.status(404).end();
 });
