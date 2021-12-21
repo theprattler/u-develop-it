3475fcaf53e6618f2f27342ce1886c6c7e7a9d7f
The Prattler <jmichaelsmith@juno.com>
2021-12-21 13:03:15 -0500

    applied some modularization

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Shaggy307!',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+module.exports = db;
